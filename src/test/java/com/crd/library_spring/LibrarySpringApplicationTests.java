99a6220d030dda7d5b65d7855ae43e8ffe1ae41c
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2025-06-06 14:55:58 +0200

    initial commit

---
+++ src/test/java/com/crd/library_spring/LibrarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crd.library_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
